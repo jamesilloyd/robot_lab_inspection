7b1edd99ee66b02d32ef23d302d3a769bd9b1e74
bdm-31 <74330144+bdm-31@users.noreply.github.com>
2020-11-20 10:52:19 +0000

    updated image cropping to account for missing parts

--- TemplateMatching/template_matching.py
+++ TemplateMatching/template_matching.py
@@ -22,10 +22,16 @@ def templateMatching(img_bgr, img_template):
 	img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
 	temp_w, temp_h = img_template.shape[::-1]
-	#print(w, h)
+	#print(temp_w, temp_h)
+
+	temp_ratio = temp_w / temp_h
+	if temp_ratio < 1.9:
+		threshold = 0.9
+	else:
+		threshold = 0.95
 
 	res = cv2.matchTemplate(img_gray,img_template,cv2.TM_CCOEFF_NORMED)
-	threshold = 0.95
+	#threshold = 0.95
 	loc = np.where( res >= threshold)
 
 
@@ -55,11 +61,21 @@ def templateMatching(img_bgr, img_template):
 	plt.axis('off')
 	plt.show()
 
-	return pt_list, temp_w, temp_h
+	return pt_list
 
 #image cropping function crops image to matched template locations
 # Returns the cropped image to be used by contouring
-def imageCropping(img_bgr, match_list, temp_w, temp_h):
+def imageCropping(img_bgr, img_template, match_list):
+
+	temp_w, temp_h = img_template.shape[::-1]
+
+	temp_ratio = temp_w / temp_h
+	if temp_ratio < 1.9:
+		total_temp_w = temp_w * 1.378
+		total_temp_h = temp_h * 1.351
+	else:
+		total_temp_w = temp_w * 1.097
+		total_temp_h = temp_h * 1.205
 
 	x_list = []
 	y_list = []
@@ -69,10 +85,49 @@ def imageCropping(img_bgr, match_list, temp_w, temp_h):
 
 	#print(x_list, y_list)
 
-	x_min = min(x_list) - 5
-	x_max = max(x_list) + temp_w + 5
-	y_min = min(y_list) - 5
-	y_max = max(y_list) + temp_h + 5
+	x_min = min(x_list)
+	y_min = min(y_list)
+	x_max = max(x_list)
+	y_max = max(y_list)
+
+	#print(x_min, y_min, x_max, y_max)
+
+	if x_min < 160:
+		sub_x = 0
+	elif x_min < 320:
+		sub_x = 1
+	else:
+		sub_x = 2
+
+	if y_min < 100:
+		sub_y = 0
+	elif y_min < 200:
+		sub_y = 1
+	elif y_min < 300:
+		sub_y = 2
+	else:
+		sub_y = 3
+
+	if x_max > 320:
+		add_x = 0
+	elif x_max > 160:
+		add_x = 1
+	else:
+		add_x = 2
+
+	if y_max > 300:
+		add_y = 0
+	elif y_max > 200:
+		add_y = 1
+	elif y_max > 100:
+		add_y = 2
+	else: 
+		add_y = 3
+
+	x_min = int(x_min - sub_x*total_temp_w - 5)
+	y_min = int(y_min - sub_y*total_temp_h - 5)
+	x_max = int(x_max + add_x*total_temp_w + temp_w + 5)
+	y_max = int(y_max + add_y*total_temp_h + temp_h + 5)
 
 	img_crop = img_bgr[y_min:y_max, x_min:x_max]
 
